ea7ac1ce3d7bd10fad329b60f03d8b0b92f5e3d7
actionchain <actionchain@chainguard.dev>
2026-05-12 21:12:23 +0000

    Update python-semantic-release/upload-to-gh-release@v9.8.9

---
+++ action.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+deprecation_message="This action has been DEPRECATED. Please use the 'python-semantic-release/publish-action' instead."
+printf >&2 '%s\n' "::warning::$deprecation_message"
+
+explicit_run_cmd() {
+  local cmd="$*"
+  printf '%s\n' "$> $cmd"
+  eval "$cmd"
+}
+
+# See https://github.com/actions/runner-images/issues/6775#issuecomment-1409268124
+# and https://github.com/actions/runner-images/issues/6775#issuecomment-1410270956
+git config --system --add safe.directory "*"
+
+# Change to configured directory
+cd "${INPUT_DIRECTORY}"
+
+# Make Token available as a correctly-named environment variables
+export GH_TOKEN="${INPUT_GITHUB_TOKEN}"
+
+# Bash array to store publish arguments
+PUBLISH_ARGS=()
+
+# Add publish arguments as necessary
+if [ -n "${INPUT_TAG}" ]; then
+  PUBLISH_ARGS+=("--tag ${INPUT_TAG}")
+fi
+
+# Run Semantic Release
+explicit_run_cmd "$PSR_VENV_BIN/semantic-release ${INPUT_ROOT_OPTIONS} publish ${PUBLISH_ARGS[*]}"
